171667e6e72681fe15808baeaa980cd7049fac5f
Gabriel <gabrieloliveira140208@gmail.com>
2026-06-15 22:30:04 -0300

    js criado para ter animações e a funcinalidade do projeto

--- main.js
+++ main.js
@@ -1,1 +1,79 @@
-// Arquivo para código javascript
+var API_URL = 'https://6a30923ca7f8866418d624b1.mockapi.io/almox-saude/materiais';
+
+function carregarMateriais() {
+    fetch(API_URL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (dados) {
+            renderTabela(dados);
+        });
+}
+
+document.getElementById('btn-cadastrar').addEventListener('click', function () {
+
+    var nome = document.getElementById('input-nome').value;
+    var quantidade = document.getElementById('input-quantidade').value;
+    var unidade = document.getElementById('input-unidade').value;
+    var msg = document.getElementById('mensagem');
+
+    if (nome == '' || quantidade == '') {
+        msg.className = 'erro';
+        msg.style.display = 'block';
+        msg.textContent = 'Preencha o nome e a quantidade antes de cadastrar.';
+        return;
+    }
+
+    var novoMaterial = {
+        nome: nome,
+        quantidade: quantidade,
+        unidade: unidade
+    };
+
+    fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(novoMaterial)
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function () {
+
+            document.getElementById('input-nome').value = '';
+            document.getElementById('input-quantidade').value = '';
+            document.getElementById('input-unidade').value = '';
+
+            msg.className = 'ok';
+            msg.style.display = 'block';
+            msg.textContent = nome + ' cadastrado com sucesso!';
+
+            carregarMateriais();
+
+        });
+
+});
+
+function renderTabela(materiais) {
+
+    var tbody = document.getElementById('lista-materiais');
+    tbody.innerHTML = '';
+
+    if (materiais.length == 0) {
+        tbody.innerHTML = '<tr><td colspan="4" class="empty">Nenhum material cadastrado ainda.</td></tr>';
+        return;
+    }
+
+    for (var i = 0; i < materiais.length; i++) {
+        var tr = document.createElement('tr');
+        tr.innerHTML =
+            '<td>' + (i + 1) + '</td>' +
+            '<td>' + materiais[i].nome + '</td>' +
+            '<td>' + materiais[i].quantidade + '</td>' +
+            '<td>' + materiais[i].unidade + '</td>';
+        tbody.appendChild(tr);
+    }
+
+}
+
+carregarMateriais();
